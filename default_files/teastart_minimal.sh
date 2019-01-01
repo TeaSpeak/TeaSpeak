33ef4fb9973cc2eb322349b1785c10097c720c6e
WolverinDEV <git@mcgalaxy.de>
2019-01-01 23:54:48 +0100

    improved minimal script

--- default_files/teastart_minimal.sh
+++ default_files/teastart_minimal.sh
@@ -25,36 +25,39 @@ if [ "$EUID" -ne 0 ]; then
     fi
 fi
 
-for packageManager in "${packageManagersList[@]}"; do
-    # split string by delimiter and replace "%" with package name
-    IFS=';' read -r -a packageManagerData <<< $packageManager
-    packageManagerData[1]="${packageManagerData[1]//%/${packageManagerData[4]}}"
-    packageManagerData[3]="${packageManagerData[3]//%/${packageManagerData[4]}}"
-    # check if package manager is available
-    if command -v ${packageManagerData[0]} > /dev/null 2>&1; then
-        # check if required package is installed
-        if ! eval "${packageManagerData[1]}" > /dev/null 2>&1; then
-            printf "\nWe're missing a required package!\n"
-            printf "Please install '${packageManagerData[4]}' (${packageManagerData[3]})\n"
-            read -r -p "Should we do this for you? [Y/n]: " response
-            response=${response,,}
-            if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
-                # install package
-                eval "${root//%/${packageManagerData[2]}}"
-                eval "${root//%/${packageManagerData[3]}}"
-                ./$0
+#Test if we've already libnice manually installed
+if [ ! -e libs/libnice.so.10 ]; then
+    for packageManager in "${packageManagersList[@]}"; do
+        # split string by delimiter and replace "%" with package name
+        IFS=';' read -r -a packageManagerData <<< $packageManager
+        packageManagerData[1]="${packageManagerData[1]//%/${packageManagerData[4]}}"
+        packageManagerData[3]="${packageManagerData[3]//%/${packageManagerData[4]}}"
+        # check if package manager is available
+        if command -v ${packageManagerData[0]} > /dev/null 2>&1; then
+            # check if required package is installed
+            if ! eval "${packageManagerData[1]}" > /dev/null 2>&1; then
+                printf "\nWe're missing a required package!\n"
+                printf "Please install '${packageManagerData[4]}' (${packageManagerData[3]})\n"
+                read -r -p "Should we do this for you? [Y/n]: " response
+                response=${response,,}
+                if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+                    # install package
+                    eval "${root//%/${packageManagerData[2]}}"
+                    eval "${root//%/${packageManagerData[3]}}"
+                    ./$0
+                fi
+                exit 0
             fi
-            exit 0
+            break;
         fi
-        break;
-    fi
-    # Couldn't find a supported package manager
-    if [ "$packageManager" = "${packageManagersList[-1]}" ]; then
-        printf '\nYour package manager is not supported!\n'
-        printf 'Please report this problem to\n'
-        printf 'https://github.com/TeaSpeak/TeaSpeak/issues\n\n'
-    fi
-done
+        # Couldn't find a supported package manager
+        if [ "$packageManager" = "${packageManagersList[-1]}" ]; then
+            printf '\nYour package manager is not supported!\n'
+            printf 'Please report this problem to\n'
+            printf 'https://github.com/TeaSpeak/TeaSpeak/issues\n\n'
+        fi
+    done
+fi
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH;./libs/"
 export LD_PRELOAD="./libs/libjemalloc.so.2"
