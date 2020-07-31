32b1ef973d44c668fc2e74735491db3896a32c2d
WolverinDEV <git@teaspeak.de>
2020-07-31 23:20:39 +0200

    Uncommented base file readlink command

--- default_files/teastart.sh
+++ default_files/teastart.sh
@@ -3,7 +3,7 @@
 TERMINAL_PIPE="/tmp/teaspeak_\${pid}_\${direction}.term"
 COMMANDLINE_PARAMETERS="${@:2}" #add any command line parameters you want to pass here
 
-#file_base=$(readlink -f "$0")
+file_base=$(readlink -f "$0")
 file_base="$0"
 directory_base=$(dirname ${file_base})
 cd ${directory_base} || exit 1
